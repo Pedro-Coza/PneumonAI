db72c761fbb2aa7f7f71345670e6de0395484933
PedroCoza <56022203+PedroCoza@users.noreply.github.com>
2020-07-20 22:49:49 +0200

    Volley Singleton added for request queues.

--- app/src/main/java/com/pneumonai/pneumonai/MainActivity.java
+++ app/src/main/java/com/pneumonai/pneumonai/MainActivity.java
@@ -1,84 +1,129 @@
 package com.pneumonai.pneumonai;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import java.io.IOException;
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
-
-public class MainActivity extends AppCompatActivity {
 
 
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
+
+    private static final String TAG = MainActivity.class.toString();
+
+    private Button btnSendRequest;
+
+    private RequestQueue mRequestQueue;
+
+    private StringRequest stringRequest;
+
+    //private TextView text = findViewById(R.id.responseText);
+
+    private Button ChooseImage, UploadImage;
+    private EditText Name;
+    private ImageView ImageView;
+    private final int IMG_REQUEST = 1;
+
+    private String url = "https://www.google.com    ";
+
+    @Override
+    protected void onCreate(Bundle SavedInstanceState) {
+        super.onCreate(SavedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void connectServer(View home){
-        EditText ipv4AddressView = findViewById(R.id.IPAddress);
-        String ipv4Address = ipv4AddressView.getText().toString();
-        EditText portNumberView = findViewById(R.id.portNumber);
-        String portNumber = portNumberView.getText().toString();
-
-        String postUrl= "http://"+ipv4Address+":"+portNumber+"/";
-
-        String postBodyText="Hello";
-        MediaType mediaType = MediaType.parse("text/plain; charset=utf-8");
-        RequestBody postBody = RequestBody.create(mediaType, postBodyText);
-
-        postRequest(postUrl, postBody);
-    }
-
-    public void postRequest(String postUrl, RequestBody postBody) {
-
-        OkHttpClient client = new OkHttpClient();
-
-        Request request = new Request.Builder()
-                .url(postUrl)
-                .post(postBody)
-                .build();
-
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                // Cancel the post on failure.
-                call.cancel();
-
-                // In order to access the TextView inside the UI thread, the code is executed inside runOnUiThread()
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        TextView responseText = findViewById(R.id.responseText);
-                        responseText.setText("Failed to Connect to Server");
-                    }
-                });
-            }
+        UploadImage = (Button)findViewById(R.id.UploadImage);
+        ChooseImage = (Button)findViewById(R.id.ChooseImage);
+        Name = (EditText) findViewById(R.id.Name);
+        ImageView = (ImageView)findViewById(R.id.ImageView);
+        ChooseImage.setOnClickListener(this);
+        UploadImage.setOnClickListener(this);
+        btnSendRequest = (Button) findViewById(R.id.btnSendRequest);
+        btnSendRequest.setOnClickListener(new View.OnClickListener(){
 
             @Override
-            public void onResponse(Call call, final Response response) throws IOException {
-                // In order to access the TextView inside the UI thread, the code is executed inside runOnUiThread()
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        TextView responseText = findViewById(R.id.responseText);
-                        try {
-                            responseText.setText(response.body().string());
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                });
+            public void onClick(View view) {
+                //send request and print response using volley
+                sendRequestAndPrintResponse();
             }
         });
+    }
+
+    private void sendRequestAndPrintResponse() {
+
+        mRequestQueue = Volley.newRequestQueue(this);
+        stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(final String response) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        TextView responseText = findViewById(R.id.responseText);
+                        responseText.setText("de lokos chavales");
+                    }
+                });
+
+                Log.i(TAG,"Response: " + response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i(TAG,"Error: " + error.toString());
+            }
+        });
+
+        mRequestQueue.add(stringRequest);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.ChooseImage:
+                selectImage();
+                break;
+
+
+        }
+    }
+
+    private void selectImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, IMG_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==IMG_REQUEST && resultCode== RESULT_OK && data!=null){
+            Uri path = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), path);
+                ImageView.setImageBitmap(bitmap);
+                ImageView.setVisibility(View.VISIBLE);
+                Name.setVisibility(View.VISIBLE);
+            }catch (IOException e){
+                e.getMessage();
+                e.printStackTrace();
+            }
+        }
     }
 }
