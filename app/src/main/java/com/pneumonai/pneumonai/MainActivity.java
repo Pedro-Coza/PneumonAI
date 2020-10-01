a978e68e1c9ba38d7d4e65408f3c8d0c6c25e51d
PedroCoza <56022203+PedroCoza@users.noreply.github.com>
2020-10-01 12:17:26 +0200

    PneumonAI Beta

--- app/src/main/java/com/pneumonai/pneumonai/MainActivity.java
+++ app/src/main/java/com/pneumonai/pneumonai/MainActivity.java
@@ -1,6 +1,9 @@
 package com.pneumonai.pneumonai;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
@@ -11,18 +14,33 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.loopj.android.http.*;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import cz.msebera.android.httpclient.Header;
 
 
 public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
@@ -41,8 +59,11 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
     private EditText Name;
     private ImageView ImageView;
     private final int IMG_REQUEST = 1;
+    private String filePath;
+    private Bitmap bitmap;
+    private static final int REQUEST_PERMISSIONS = 100;
 
-    private String url = "https://www.google.com    ";
+    private static final String URL = "https://servidor-definitivo.ew.r.appspot.com";
 
     @Override
     protected void onCreate(Bundle SavedInstanceState) {
@@ -53,8 +74,18 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
         ChooseImage = (Button)findViewById(R.id.ChooseImage);
         Name = (EditText) findViewById(R.id.Name);
         ImageView = (ImageView)findViewById(R.id.ImageView);
-        ChooseImage.setOnClickListener(this);
-        UploadImage.setOnClickListener(this);
+        ChooseImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectImage();
+            }
+        });
+        UploadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadImage(bitmap);
+            }
+        });
         btnSendRequest = (Button) findViewById(R.id.btnSendRequest);
         btnSendRequest.setOnClickListener(new View.OnClickListener(){
 
@@ -69,14 +100,14 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
     private void sendRequestAndPrintResponse() {
 
         mRequestQueue = Volley.newRequestQueue(this);
-        stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+        stringRequest = new StringRequest(Request.Method.GET, URL, new Response.Listener<String>() {
             @Override
             public void onResponse(final String response) {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
                         TextView responseText = findViewById(R.id.responseText);
-                        responseText.setText("de lokos chavales");
+                        responseText.setText(response.toString());
                     }
                 });
 
@@ -94,12 +125,25 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
 
     @Override
     public void onClick(View view) {
+        if ((ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) && (ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)) {
+            if ((ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) && (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE))) {
+
+            } else {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE},
+                        REQUEST_PERMISSIONS);
+            }
+        } else {
+            Log.e("Else", "Else");
+            selectImage();
+        }
         switch (view.getId()){
-            case R.id.ChooseImage:
-                selectImage();
-                break;
-
-
+            case R.id.UploadImage:
+                uploadImage(bitmap);
         }
     }
 
@@ -110,20 +154,85 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
         startActivityForResult(intent, IMG_REQUEST);
     }
 
+
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode==IMG_REQUEST && resultCode== RESULT_OK && data!=null){
-            Uri path = data.getData();
-            try {
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), path);
-                ImageView.setImageBitmap(bitmap);
-                ImageView.setVisibility(View.VISIBLE);
-                Name.setVisibility(View.VISIBLE);
-            }catch (IOException e){
-                e.getMessage();
-                e.printStackTrace();
+            Uri uri = data.getData();
+            filePath = getPath(uri);
+            if(filePath != null) {
+                try {
+                    ImageView.setVisibility(View.VISIBLE);
+                    Name.setVisibility(View.VISIBLE);
+                    bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                    ImageView.setImageBitmap(bitmap);
+                    Log.d("filePath", String.valueOf(filePath));
+                } catch (IOException e) {
+                    e.getMessage();
+                    e.printStackTrace();
+                }
             }
+            else {
+                Toast.makeText(MainActivity.this,"no image selected", Toast.LENGTH_LONG).show(); }
         }
+    }
+
+    public String getPath(Uri uri) {
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+        cursor.moveToFirst();
+        String document_id = cursor.getString(0);
+        document_id = document_id.substring(document_id.lastIndexOf(":") + 1);
+        cursor.close();
+
+        cursor = getContentResolver().query(
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                null, MediaStore.Images.Media._ID + " = ? ", new String[]{document_id}, null);
+        String path = "";
+        if(cursor !=null && cursor.moveToFirst()) {
+            path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            cursor.close();
+        }
+
+        return path;
+    }
+
+    public byte[] getFileDataFromDrawable(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 80, byteArrayOutputStream);
+        return byteArrayOutputStream.toByteArray();
+    }
+
+    private void uploadImage(final Bitmap bitmap) {
+
+        VolleyMultipartRequest volleyMultipartRequest = new VolleyMultipartRequest(Request.Method.POST, URL,
+                new Response.Listener<NetworkResponse>() {
+                    @Override
+                    public void onResponse(NetworkResponse response) {
+                            String resp = new String(response.data);
+                            Toast.makeText(getApplicationContext(), resp, Toast.LENGTH_SHORT).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+                        Log.e("GotError",""+error.getMessage());
+                    }
+                }) {
+
+
+            @Override
+            protected Map<String, DataPart> getByteData() {
+                Map<String, DataPart> params = new HashMap<>();
+                long imagename = System.currentTimeMillis();
+                params.put("image", new DataPart(imagename + ".png", getFileDataFromDrawable(bitmap)));
+                return params;
+            }
+        };
+
+        //adding the request to volley
+        Volley.newRequestQueue(this).add(volleyMultipartRequest);
     }
 }
